e5b955eb34b6945fe3df036c97cbe4ce039744d4
heipartho05 <heipartho72@gmail.com>
2022-02-20 21:03:36 +0600

    Initial commit

---
+++ src/com/partho/BubbleSort.java
@@ -0,0 +1,36 @@
+package com.partho;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr) {
+        boolean swapped;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            swapped = false;
+
+            for (int j = 1; j < arr.length - i; j++) {
+
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // if did not swap it means array is sorted hence end the program
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
